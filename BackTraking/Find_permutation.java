42012e6bad3079ec8f8ff7d7f0f5f1db30a7be16
subha <subhadipp26@gmail.com>
2024-09-21 20:49:43 +0530

    n_queen

--- BackTraking/Find_permutation.java
+++ BackTraking/Find_permutation.java
@@ -1,8 +1,19 @@
 public class Find_permutation {
     public static void find_permutation(String str,String ans){
-        
+        // base case
+        if(str.length()==0){
+            System.out.println(ans);
+            return;
+        }
+        // recursion
+
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+           String newStr=str.substring(0, i)+str.substring(i+1);
+            find_permutation(newStr, ans+ch);
+        }
     }
     public static void main(String[] args) {
-        
+        find_permutation("abc", "");
     }
 }
---
+++ BackTraking/NQueen.java
@@ -0,0 +1,65 @@
+public class NQueen {
+    public static boolean isSafe(char bord[][],int row,int col){
+        // vertical up
+        for(int i=row-1;i>=0;i--){
+            if(bord[i][col]=='Q'){
+                return false;
+            }
+        }
+
+        // diag left up
+
+        for(int i=row-1,j=col-1;i>=0 && j>=0 ;i--,j--){
+            if(bord[i][j]=='Q'){
+                return false;
+            }
+        }
+        // diag right up
+
+        for(int i=row-1,j=col+1;i>=0 && j<bord.length;i--,j++){
+            if(bord[i][j]=='Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void nQueen(char bord[][],int row){
+        // base case
+        if(row==bord.length){
+            printBord(bord);
+            return;
+        }
+
+        for(int i=0;i<bord.length;i++){
+            if(isSafe(bord,row,i)){
+                bord[row][i]='Q';
+                nQueen(bord, row+1);// recursion
+                bord[row][i]='Y'; // back traking
+            }
+           
+           
+        }
+    }
+    public static void printBord(char bord[][]){
+        System.out.println("-------chese bord---------");
+        for(int i=0;i<bord.length;i++){
+            for(int j=0;j<bord.length;j++){
+                System.out.print(bord[i][j]+" ");
+
+            }
+            System.out.println();
+        }
+
+    }
+    public static void main(String[] args) {
+        int n=4;
+        char [][] bord=new char[n][n];
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                bord[i][j]='x';
+            }
+        }
+        nQueen(bord,0);
+    }
+}
